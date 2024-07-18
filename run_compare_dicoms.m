023548543828d4e351930d756c68e738cebff51e
awilzman <49848685+awilzman@users.noreply.github.com>
2024-07-18 13:16:47 -0400

    update saving

--- run_compare_dicoms.m
+++ run_compare_dicoms.m
@@ -1,12 +1,10 @@
 clear %save data before starting
 directory = ''; %end slash is important
 res = 82;
-calibrate_slope = 2.63;
-calibrate_int = 15.93;
-%calibrate_slope = 0.000357;
-%calibrate_int = -0.0012625; 
+calibrate_slope = 0.000357;
+calibrate_int = -0.0012625; 
 
-subjects = ["Test-"];
+subjects = ["MARA14-"];
 LCV_name = 'LCV';
 mask1_name = 'SCAN1';
 mask2_name = 'SCAN2';
@@ -37,15 +35,30 @@ output = [labels,output];
 % Export to Excel
 excelFileName = 'output.xlsx';
 % Loop through each subject
-for j = 2:length(subjects)+1
-    % Write the file name
-    writematrix(output{1, 2}, excelFileName, 'Sheet', 1, 'Range', ['A' num2str(18*(j-2)+3)]);
+allData = {};
+
+for j = 2:length(subjects) + 1
+    % Calculate the starting row for this data block
+    baseRow = 18 * (j - 2) + 3;
+    % Write the file name in the first row of the block
+    allData{baseRow, 1} = output{1, 2}; % Store the file name in the first column
     for i = 2:5
         % Calculate the starting row for this data block
-        startRow = (18 * (j - 2)) + (i - 1) * 4;
-        label = output{i,1};
-        dataMatrix = output{i,2};
-        writematrix(label, excelFileName, 'Sheet', 1, 'Range', ['A' num2str(startRow)]);
-        writematrix(dataMatrix, excelFileName, 'Sheet', 1, 'Range', ['B' num2str(startRow)]);
+        startRow = baseRow + (i - 1) * 4;
+        label = output{i, 1};
+        dataMatrix = output{i, 2};
+        numDataRows = size(dataMatrix, 1);
+        numDataCols = size(dataMatrix, 2);
+        % Store the label
+        allData{startRow, 1} = label;
+        % Store the data matrix row-wise
+        for r = 1:numDataRows
+            for c = 1:numDataCols
+                allData{startRow + r - 1, 1 + c} = dataMatrix(r, c);
+            end
+        end
     end
 end
+
+% Write the collected data to the Excel file in one go
+writecell(allData, excelFileName, 'Sheet', 1);
