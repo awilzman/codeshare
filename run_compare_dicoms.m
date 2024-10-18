789ba786ae17ca38465afa7bb9cdc433f5a5d7b3
awilzman <49848685+awilzman@users.noreply.github.com>
2024-10-18 09:28:12 -0400

    workingonit

--- run_compare_dicoms.m
+++ run_compare_dicoms.m
@@ -1,7 +1,7 @@
 clear;  % Clear workspace
 
 % Initial Setup
-subs = ["15"];  % List of subjects
+subs = ["28","15"];  % List of subjects
 directory = '';  % Directory of DICOM files (end slash is important)
 res = 82;  % Image resolution (in micrometers)
 calibrate_slope = 0.000357;
@@ -19,23 +19,28 @@ mask1s = cell(length(subs), 2);  % Preallocate for mask1s
 mask2s = cell(length(subs), 2);  % Preallocate for mask2s
 
 % Loop through subjects
-medial_left = 0;
-angle_rot = 0;
+
+
 for s = 1:length(subs)
     subjects = strcat(subs(s), [" 4 ", " 30 "]);
-    
+    angle_rot = 0;
+    medial_left = 0;
     % Prepare file names and compare DICOMs
     for i = 1:length(subjects)
         output{s, i, 1} = strcat(subjects(i), 'lcv');  % Scan name
         mask1s{s, i} = strcat(subjects(i), 'scan1');   % Mask 1 name
         mask2s{s, i} = strcat(subjects(i), 'scan2');   % Mask 2 name
-
+         
         disp("Running " + subjects(i));  % Log current subject
         
         % Compare DICOMs and store results
-        [output{s, i, 2}, output{s, i, 3}, output{s, i, 4}, output{s, i, 5}] = ...
-            compare_dicoms(directory, res, output{s, i, 1}, mask1s{s, i}, mask2s{s, i}, ...
-            calibrate_slope, calibrate_int, medial_left, angle_rot);
+        try
+            [output{s, i, 2}, output{s, i, 3}, output{s, i, 4}, output{s, i, 5}] = ...
+                compare_dicoms(directory, res, output{s, i, 1}, mask1s{s, i}, mask2s{s, i}, ...
+                calibrate_slope, calibrate_int, medial_left, angle_rot);
+        catch
+            disp(['No data for ' + subjects(i)]);
+        end
     end
 end
 
